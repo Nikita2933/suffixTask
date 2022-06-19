fd612434bac96eefb7b7d12abca92662bb5a350f
nikita.ivanov <nikita.ivanov@btc-s.ru>
2022-06-19 11:44:52 +0300

    Initial Commit

---
+++ suffixTask/suffixTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  suffixTaskApp.swift
+//  suffixTask
+//
+//  Created by Никита Иванов on 19.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct suffixTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
